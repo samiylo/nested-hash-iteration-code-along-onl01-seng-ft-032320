30a95062f898958b5d249f24602a429ffe81ea31
Samiylo Kryshu <skryshu@gmail.com>
2020-03-27 18:33:40 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,16 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_info|
+    
+    if person == "Freddy Mercury"
+      contact_info.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+        
+      end
+    end
+  end 
+  
 end
